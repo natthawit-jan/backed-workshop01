4fa11c3ea7447c0ff550ac6bfe81db0cafe452f1
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-03 11:54:03 +0700

    fix naming issue in HelloWorldTest.java

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -5,14 +5,14 @@ import static org.junit.Assert.*;
 
 public class HelloWorldTest {
     @Test
-    public void sayHi_should_return_hello_World() {
+    public void sayHiTo_world_should_return_hello_World() {
         HelloWorld helloWorld = new HelloWorld();
         String result = helloWorld.sayHiTo("world");
         assertEquals("Hello World", result);
     }
 
 
-    public void sayHi_should_return_Hello_Natthawit() {
+    public void sayHiTo_natthawit_should_return_Hello_Natthawit() {
         HelloWorld helloWorld = new HelloWorld();
         String result = helloWorld.sayHiTo("natthawit");
         assertEquals("Hello Natthawit", result);
