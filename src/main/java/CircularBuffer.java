5d7c039245c9bcd940eb646d8099c209d7f06d51
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-03 22:16:23 +0700

    [ADDED] CircularBuffer.java

---
+++ src/main/java/CircularBuffer.java
@@ -0,0 +1,51 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class CircularBuffer<T> {
+
+    private final int BUFFER_SIZE = 5;
+    private int countElement;
+    private final List<T> buffer;
+    private int readPointer;
+    private int writePointer;
+
+    public CircularBuffer() {
+        buffer = new LinkedList<>();
+        countElement = 0;
+        readPointer = 0;
+        writePointer = 0;
+    }
+
+    public boolean isEmpty() {
+        return countElement == 0;
+    }
+
+    public void writeData(T s) {
+        buffer.add(writePointer, s);
+        if (isFull()) {
+            incrementReadPointer();
+        } else {
+            countElement++;
+        }
+        incrementWritePointer();
+    }
+
+    private void incrementWritePointer() {
+        writePointer = (writePointer + 1) % BUFFER_SIZE;
+    }
+
+    private void incrementReadPointer() {
+        readPointer = (readPointer + 1) % BUFFER_SIZE;
+    }
+
+    public boolean isFull() {
+        return writePointer == readPointer && countElement == BUFFER_SIZE;
+    }
+
+    public T readData() {
+        T result = buffer.get(readPointer);
+        incrementReadPointer();
+        countElement--;
+        return result;
+    }
+}
