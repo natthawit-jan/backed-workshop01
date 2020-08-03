dcb1a77e5c95d8a3c3acb4167d40c6d234bf0424
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-03 21:45:58 +0700

    TC4

--- src/test/java/CircularBufferTest.java
+++ src/test/java/CircularBufferTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CircularBufferTest {
@@ -14,19 +15,37 @@ public class CircularBufferTest {
     @Test
     public void newly_created_buffer_should_be_five() {
         CircularBuffer<String> circularBuffer = new CircularBuffer<>();
-        for (int i = 0; i < 5; i++ ){
+        for (int i = 0; i < 5; i++) {
             circularBuffer.writeData("A" + i);
         }
         assertTrue("The buffer is not full, meaning that the bufferSize is not 5", circularBuffer.isFull());
     }
 
     @Test
-    public void writing_A_to_buffer_should_read_A(){
+    public void writing_A_to_buffer_should_read_A() {
         CircularBuffer<String> circularBuffer = new CircularBuffer<>();
         circularBuffer.writeData("A");
         circularBuffer.writeData("B");
         assertEquals("The retrieved data is not 'A'", "A", circularBuffer.readData());
         assertEquals("The retrieved data is not 'B'", "B", circularBuffer.readData());
+    }
+
+    @Test
+    public void write_pointer_should_wrap_around_the_buffer() {
+        CircularBuffer<String> circularBuffer = new CircularBuffer<>();
+        for (int i = 0; i < 4; i++) {
+            circularBuffer.writeData("A" + i);
+        }
+        assertEquals("The first read is not A0", "A0", circularBuffer.readData());
+        //the writer pointer goes back
+
+        circularBuffer.writeData("A4");
+        circularBuffer.writeData("B0"); //Read pointer now is the same as write pointer now
+        String result = null;
+        for (int i = 0; i < 5; i++) {
+            result = circularBuffer.readData();
+        }
+        assertEquals("This read data is not B0", "B0", result);
     }
 
 }
